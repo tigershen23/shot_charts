f5144b746ea6f9cdc403ed5c2848d55c4f32624e
tigershen23 <tigershen23@gmail.com>
2015-08-14 16:57:10 -0700

    Get data from URL

--- nba_player.py
+++ nba_player.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-
+# James Harden's 2014-2015 shot chart data URL
 shot_chart_url = 'http://stats.nba.com/stats/shotchartdetail?CFID=33&CFPAR'\
                 'AMS=2014-15&ContextFilter=&ContextMeasure=FGA&DateFrom=&D'\
                 'ateTo=&GameID=&GameSegment=&LastNGames=0&LeagueID=00&Loca'\
@@ -13,3 +13,8 @@ shot_chart_url = 'http://stats.nba.com/stats/shotchartdetail?CFID=33&CFPAR'\
                 'onSegment=&SeasonType=Regular+Season&TeamID=0&VsConferenc'\
                 'e=&VsDivision=&mode=Advanced&showDetails=0&showShots=1&sh'\
                 'owZones=0'
+
+# Get data from URL
+response = requests.get(shot_chart_url)
+headers = response.json()['resultSets'][0]['headers']
+shots = response.json()['resultSets'][9]['rowSet']
